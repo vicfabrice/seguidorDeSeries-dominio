4d79c844d57b38f856de6caad125ab0a66e912ab
vicfabrice <discosdevictoria@gmail.com>
2017-12-06 23:14:13 -0300

    primer commit del proyecto arena Seguidor de Series

---
+++ seguidorDeSeries-arena/src/main/java/seguidorDeSeries_arena/seguidorDeSeries_arena/App.java
@@ -0,0 +1,13 @@
+package seguidorDeSeries_arena.seguidorDeSeries_arena;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ seguidorDeSeries-dominio/src/main/java/seguidorDeSeries_dominio/seguidorDeSeries_dominio/App.java
@@ -0,0 +1,13 @@
+package seguidorDeSeries_dominio.seguidorDeSeries_dominio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
